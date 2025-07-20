fbd8df118a52bd680448916565c5d4976235c2fa
Parthiv Saikia <parthivsaikia0226@gmail.com>
2025-07-21 00:54:30 +0530

    Added Trie

---
+++ templates/Trie.cpp
@@ -0,0 +1,78 @@
+struct Node {
+    Node* links[26];
+    bool flag = false;
+
+    bool containsKey(char ch) {
+        return links[ch - 'a'] != NULL;
+    }
+
+    void put(char ch, Node* node) {
+        links[ch - 'a'] = node;
+    }
+
+    Node* get(char ch) {
+        return links[ch - 'a'];
+    }
+
+    void setEnd() {
+        flag = true;
+    }
+
+    bool isEnd() {
+        return flag;
+    }
+};
+
+class Trie {
+private:
+    Node* root;
+
+public:
+    Trie() {
+        root = new Node();
+    }
+
+    void insert(string word) {
+        Node* node = root;
+        for (int i = 0; i < word.length(); i++) {
+            if (!node->containsKey(word[i])) {
+                node->put(word[i], new Node());
+            }
+            node = node->get(word[i]);
+        }
+        node->setEnd();
+    }
+
+    bool search(string word) {
+        Node* node = root;
+        for (int i = 0; i < word.length(); i++) {
+            if (!node->containsKey(word[i])) {
+                return false;
+            }
+            node = node->get(word[i]);
+        }
+        return node->isEnd();
+    }
+
+    bool startsWith(string prefix) {
+        Node* node = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            if (!node->containsKey(prefix[i])) {
+                return false;
+            }
+            node = node->get(prefix[i]);
+        }
+        return true;
+    }
+};
+
+
+
+// Use Trie trie;
+// trie.insert("striver");
+
+//cout << "Search if Strike exists in trie: " <<
+// ( trie.search("strike") ? "True" : "False" )<< endl;
+
+//cout << "If words is Trie start with Stri: " <<
+//(trie.startsWith("stri") ? "True" : "False" )<< endl;
